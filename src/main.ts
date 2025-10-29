a9f79699fe507bbe4140cca3cbef8220e2a008ae
Viet An <tiktaktictac269@gmail.com>
2025-10-29 14:30:11 +0700

    feat: render a 2d plane

--- src/main.ts
+++ src/main.ts
@@ -36,16 +36,16 @@ function main() {
 		return width / height;
 	}
 
-	function addCam1() {
+	function addMainCam() {
 		const left = -1;
 		const right = 1;
 		const top = 1;
 		const bottom = -1;
 		const near = 0.1;
-		const far = 100;
+		const far = 70;
 		const camera1 = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
-		camera1.zoom = 0.08;
-		camera1.position.set(10, 20, 40);
+		camera1.zoom = 0.03;
+		camera1.position.set(5, 5, 50); // 0,0 would be center - 5,5 to be slightly off
 
 		const controls = new OrbitControls(camera1, view1Elem);
 		controls.update();
@@ -53,7 +53,7 @@ function main() {
 		return camera1;
 	}
 
-	function addCam2() {
+	function addMetaCam() {
 		const camera2 = new THREE.PerspectiveCamera(50, 1, 0.1, 500);
 		camera2.position.set(120, 20, 70);
 
@@ -63,21 +63,22 @@ function main() {
 	}
 
 	function addHelpers() {
-		const cameraHelper = new THREE.CameraHelper(camera1);
+		const cameraHelper = new THREE.CameraHelper(mainCam);
 		scene.add(cameraHelper);
 		const axesHelper = new THREE.AxesHelper(10);
 		scene.add(axesHelper);
-		const gridHelper = new THREE.GridHelper(50, 20, 0xff00ff);
+		const gridHelper = new THREE.GridHelper(50, 50, 0xff00ff);
 		gridHelper.material.transparent = true;
-		gridHelper.material.opacity = 0.5;
+		gridHelper.material.opacity = 0.3;
+		gridHelper.rotation.x = Math.PI / 2;
 		scene.add(gridHelper);
 		return { cameraHelper, axesHelper, gridHelper };
 	}
 
 	function addGUIs() {
 		const gui = new GUI();
-		gui.add(camera1, 'zoom', 0.01, 1, 0.01).listen();
-		const minMaxGUIHelper = new MinMaxGUIHelper(camera1, 'near', 'far', 0.1);
+		gui.add(mainCam, 'zoom', 0.01, 1, 0.01).listen();
+		const minMaxGUIHelper = new MinMaxGUIHelper(mainCam, 'near', 'far', 0.1);
 		gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near');
 		gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far');
 
@@ -90,20 +91,15 @@ function main() {
 		const color = 0xffffff;
 		const intensity = 3;
 		const light = new THREE.DirectionalLight(color, intensity);
-		light.position.set(0, 10, 0);
-		light.target.position.set(-5, 0, 0);
+		light.position.set(-20, 10, 10);
 		scene.add(light);
 		scene.add(light.target);
+
+		const helper = new THREE.DirectionalLightHelper(light, 5);
+		scene.add(helper);
 	}
 
 	function addObjects() {
-		const cubeSize = 3;
-		const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-		const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' });
-		const cubeMesh = new THREE.Mesh(cubeGeo, cubeMat);
-		cubeMesh.position.set(cubeSize + 1, cubeSize / 2, 0);
-		scene.add(cubeMesh);
-
 		const sphereRadius = 2;
 		const sphereWidthDivisions = 32;
 		const sphereHeightDivisions = 16;
@@ -116,10 +112,22 @@ function main() {
 		const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
 		sphereMesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
 		scene.add(sphereMesh);
+
+		const geometry = new THREE.PlaneGeometry(4, 2);
+		const material = new THREE.MeshBasicMaterial({
+			color: 'red',
+			transparent: true,
+			opacity: 0.5,
+		});
+		const plane = new THREE.Mesh(geometry, material);
+		scene.add(plane);
+		const box = new THREE.BoxHelper(plane, 0xffff00);
+		scene.add(box);
+
 	}
 
-	const camera1 = addCam1();
-	const camera2 = addCam2();
+	const mainCam = addMainCam();
+	const metaCam = addMetaCam();
 	const { cameraHelper } = addHelpers();
 	const { stats } = addGUIs();
 	addLight();
@@ -130,13 +138,13 @@ function main() {
 		const newHeight = window.innerHeight;
 
 		const view1Aspect = setScissorForElement(view1Elem); // render the original view
-		camera1.left = -view1Aspect;
-		camera1.right = view1Aspect;
-		camera1.updateProjectionMatrix();
+		mainCam.left = -view1Aspect;
+		mainCam.right = view1Aspect;
+		mainCam.updateProjectionMatrix();
 
 		renderer.setSize(newWidth, newHeight);
-		camera2.aspect = newWidth / newHeight;
-		camera2.updateProjectionMatrix();
+		metaCam.aspect = newWidth / newHeight;
+		metaCam.updateProjectionMatrix();
 	}
 
 	onWindowResize();
@@ -147,20 +155,20 @@ function main() {
 		renderer.setScissorTest(true); // turn on the scissor
 
 		const view1Aspect = setScissorForElement(view1Elem); // render the original view
-		camera1.left = -view1Aspect;
-		camera1.right = view1Aspect;
-		camera1.updateProjectionMatrix();
+		mainCam.left = -view1Aspect;
+		mainCam.right = view1Aspect;
+		mainCam.updateProjectionMatrix();
 		cameraHelper.update();
 		cameraHelper.visible = false;
 		(scene.background as THREE.Color).set(0x000000);
-		renderer.render(scene, camera1);
+		renderer.render(scene, mainCam);
 
 		const view2Aspect = setScissorForElement(view2Elem); // render from the 2nd camera
-		camera2.aspect = view2Aspect;
-		camera2.updateProjectionMatrix();
+		metaCam.aspect = view2Aspect;
+		metaCam.updateProjectionMatrix();
 		cameraHelper.visible = true;
 		(scene.background as THREE.Color).set(0x1b409e);
-		renderer.render(scene, camera2);
+		renderer.render(scene, metaCam);
 
 		stats.update();
 	}
