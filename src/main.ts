b9a53ac136f6e9fed3386c835d62b3a644722a0d
Viet An <tiktaktictac269@gmail.com>
2025-10-31 00:56:19 +0700

    feat: render basic viewport, make camera setup more proportionate, improve object handling

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,155 @@ function main() {
 	const scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xffffff);
 
+	const mult = 20;
+
+	function addMainCam() {
+		const left = -mult;
+		const right = mult;
+		const top = mult;
+		const bottom = -mult;
+		const near = 0.1;
+		const far = 15 * mult;
+		const mainCam = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+		mainCam.zoom = 0.1;
+		mainCam.position.set(0, 0, far / 2);
+
+		const controls = new OrbitControls(mainCam, view1Elem);
+		controls.zoomToCursor = true;
+		controls.mouseButtons = {
+			LEFT: THREE.MOUSE.PAN,
+			RIGHT: THREE.MOUSE.ROTATE,
+		};
+		controls.update();
+
+		return mainCam;
+	}
+
+	function addMetaCam() {
+		const metaCam = new THREE.PerspectiveCamera(50, 1, 10, 400 * mult);
+		metaCam.position.set(0, 50, 50 * mult);
+
+		const controls2 = new OrbitControls(metaCam, view2Elem);
+		controls2.update();
+		return metaCam;
+	}
+
+	function addHelpers() {
+		const cameraHelper = new THREE.CameraHelper(mainCam);
+		scene.add(cameraHelper);
+		const axesHelper = new THREE.AxesHelper(5 * mult);
+		scene.add(axesHelper);
+		const gridHelper = new THREE.GridHelper(50 * mult, 20, 0xff00ff);
+		gridHelper.material.transparent = true;
+		gridHelper.material.opacity = 0.5;
+		gridHelper.rotation.x = Math.PI / 2;
+		scene.add(gridHelper);
+		return { cameraHelper, axesHelper, gridHelper };
+	}
+
+	function addGUIs() {
+		const gui = new GUI();
+		const mainCamFolder = gui.addFolder('mainCam position');
+		mainCamFolder.add(mainCam.position, 'x', -mult * 25, mult * 25, 10).listen();
+		mainCamFolder.add(mainCam.position, 'y', -mult * 25, mult * 25, 10).listen();
+		mainCamFolder.add(mainCam.position, 'z', -mult * 25, mult * 25, 10).listen();
+		mainCamFolder.open();
+		gui.add(mainCam, 'zoom', 0.01, 1, 0.01).listen();
+		const minMaxGUIHelper = new MinMaxGUIHelper(mainCam, 'near', 'far', 0.1);
+		gui.add(minMaxGUIHelper, 'min', 0.1, mainCam.far / 2, 10).name('near');
+		gui.add(minMaxGUIHelper, 'max', mainCam.far / 2, mainCam.far, 10).name('far');
+
+		const stats = new Stats();
+		document.body.appendChild(stats.dom);
+		return { gui, stats };
+	}
+
+	function addViewports() {
+		const viewports = dxf.entities.filter((entity) => entity.type === 'VIEWPORT');
+		viewports.forEach((vp) => {
+			const aspect = vp.width / vp.height;
+			const viewHeight = vp.viewHeight;
+			const viewWidth = viewHeight * aspect;
+			const geo = new THREE.PlaneGeometry(viewWidth, viewHeight);
+			const mat = new THREE.MeshBasicMaterial({
+				color: 'lightblue',
+				side: THREE.DoubleSide,
+				transparent: true,
+				opacity: 0.5,
+			});
+			const plane = new THREE.Mesh(geo, mat);
+			plane.position.set(vp.displayCenter.x, vp.displayCenter.y, -20);
+			scene.add(plane);
+
+			const edges = new THREE.EdgesGeometry(geo);
+			const outline = new THREE.LineSegments(
+				edges,
+				new THREE.LineBasicMaterial({ color: 0x000000 }),
+			);
+			plane.add(outline);
+		});
+	}
+
+	function addObjects() {
+		const solids = dxf.entities.filter((entity) => entity.type === 'SOLID');
+		solids.forEach((solid) => {
+			const solidMesh = renderSolid(solid);
+			scene.add(solidMesh);
+		});
+		const inserts = dxf.entities.filter((entity) => entity.type === 'INSERT');
+		const colorSwatchBlock = dxf.blocks['Color Swatch'];
+		inserts.forEach((insert) => {
+			const { colorIndex } = insert;
+
+			const insertMesh = renderSolid(
+				colorSwatchBlock.entities[0],
+				insert.insertionPoint,
+				colorIndex,
+				insert.xScale,
+				undefined,
+				insert.rotation,
+			);
+			scene.add(insertMesh);
+		});
+
+		const target = dxf.entities.filter((entity) => entity.type === 'LWPOLYLINE').slice(0, 10);
+		target.forEach((polyline) => {
+			const positions: number[] = [];
+
+			polyline.vertices.forEach((v: { x: number; y: number }) => {
+				const divider = 1;
+				positions.push(v.x / divider, v.y / divider, 0);
+			});
+
+			// 1. Geometry (using Float32BufferAttribute for performance)
+			const geometry = new THREE.BufferGeometry();
+			// Convert the array to a typed array
+			const positionAttribute = new THREE.Float32BufferAttribute(positions, 3);
+			geometry.setAttribute('position', positionAttribute);
+
+			// 2. Material (using LineBasicMaterial)
+			// You can use entity data like 'layer' or 'lineType' to determine color/style
+			const material = new THREE.LineBasicMaterial({
+				color: 0xff0000,
+				// linewidth: polyline.constantWidth || 1, // Use constantWidth if applicable
+				linewidth: 1000,
+			});
+
+			// 3. Line Object
+			const line = new THREE.Line(geometry, material);
+
+			// Add to the scene
+			scene.add(line);
+		});
+	}
+
+	const mainCam = addMainCam();
+	const metaCam = addMetaCam();
+	const { cameraHelper } = addHelpers();
+	const { stats } = addGUIs();
+	addViewports();
+	addObjects();
+
 	function setScissorForElement(elem: HTMLElement) {
 		const canvasRect = canvas.getBoundingClientRect();
 		const elemRect = elem.getBoundingClientRect();
@@ -45,107 +194,14 @@ function main() {
 		// return the aspect
 		return width / height;
 	}
-
-	function addMainCam() {
-		const left = -25;
-		const right = 25;
-		const top = 25;
-		const bottom = -25;
-		const near = 0.1;
-		const far = 1000;
-		const mainCam = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
-		mainCam.zoom = 0.09;
-		mainCam.position.set(0, 0, 500); // 0,0 would be center
-
-		const controls = new OrbitControls(mainCam, view1Elem);
-		controls.zoomToCursor = true;
-		controls.mouseButtons = {
-			LEFT: THREE.MOUSE.PAN,
-			RIGHT: THREE.MOUSE.ROTATE,
-		};
-		controls.update();
-
-		return mainCam;
-	}
-
-	function addMetaCam() {
-		const metaCam = new THREE.PerspectiveCamera(50, 1, 0.1, 5000);
-		metaCam.position.set(120, 50, 1000);
-
-		const controls2 = new OrbitControls(metaCam, view2Elem);
-		controls2.update();
-		return metaCam;
-	}
-
-	function addHelpers() {
-		const cameraHelper = new THREE.CameraHelper(mainCam);
-		scene.add(cameraHelper);
-		const axesHelper = new THREE.AxesHelper(50);
-		scene.add(axesHelper);
-		const gridHelper = new THREE.GridHelper(2500, 50, 0xff00ff);
-		gridHelper.material.transparent = true;
-		gridHelper.material.opacity = 0.5;
-		gridHelper.rotation.x = Math.PI / 2;
-		scene.add(gridHelper);
-		return { cameraHelper, axesHelper, gridHelper };
-	}
-
-	function addGUIs() {
-		const gui = new GUI();
-		const mainCamFolder = gui.addFolder('mainCam position');
-		mainCamFolder.add(mainCam.position, 'x', 1, 200, 10).listen();
-		mainCamFolder.add(mainCam.position, 'y', 1, 200, 10).listen();
-		mainCamFolder.add(mainCam.position, 'z', 1, 200, 10).listen();
-		mainCamFolder.open(); // Optionally open the folder by default
-		gui.add(mainCam, 'zoom', 0.01, 1, 0.01).listen();
-		const minMaxGUIHelper = new MinMaxGUIHelper(mainCam, 'near', 'far', 0.1);
-		gui.add(minMaxGUIHelper, 'min', 0.1, 500, 10).name('near');
-		gui.add(minMaxGUIHelper, 'max', 0.1, 1000, 10).name('far');
-
-		const stats = new Stats();
-		document.body.appendChild(stats.dom);
-		return { gui, stats };
-	}
-
-	function addObjects() {
-		const solids = dxf.entities.filter((entity) => entity.type === 'SOLID');
-		solids.forEach((solid) => {
-			const solidMesh = renderSolid(solid);
-			scene.add(solidMesh);
-		});
-
-		const inserts = dxf.entities.filter((entity) => entity.type === 'INSERT');
-		const colorSwatchBlock = dxf.blocks['Color Swatch'];
-		inserts.forEach((insert) => {
-			const insertMesh = renderSolid(
-				colorSwatchBlock.entities[0],
-				insert.insertionPoint,
-				insert.colorIndex,
-				insert.xScale,
-				undefined,
-				insert.rotation,
-			);
-			scene.add(insertMesh);
-		});
-	}
-
-	const mainCam = addMainCam();
-	const metaCam = addMetaCam();
-	const { cameraHelper } = addHelpers();
-	const { stats } = addGUIs();
-	addObjects();
-
 	function onWindowResize() {
-		const newWidth = window.innerWidth;
-		const newHeight = window.innerHeight;
-
 		const view1Aspect = setScissorForElement(view1Elem); // render the original view
 		mainCam.left = -view1Aspect;
 		mainCam.right = view1Aspect;
 		mainCam.updateProjectionMatrix();
 
-		renderer.setSize(newWidth, newHeight);
-		metaCam.aspect = newWidth / newHeight;
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		metaCam.aspect = window.innerWidth / window.innerHeight;
 		metaCam.updateProjectionMatrix();
 	}
 
@@ -157,8 +213,8 @@ function main() {
 		renderer.setScissorTest(true); // turn on the scissor
 
 		const view1Aspect = setScissorForElement(view1Elem); // render the original view
-		mainCam.left = -view1Aspect * 25;
-		mainCam.right = view1Aspect * 25;
+		mainCam.left = -view1Aspect * mult;
+		mainCam.right = view1Aspect * mult;
 		mainCam.updateProjectionMatrix();
 		cameraHelper.update();
 		cameraHelper.visible = false;
@@ -169,7 +225,7 @@ function main() {
 		metaCam.aspect = view2Aspect;
 		metaCam.updateProjectionMatrix();
 		cameraHelper.visible = true;
-		(scene.background as THREE.Color).set(0x1b409e);
+		(scene.background as THREE.Color).set(0x0c1426);
 		renderer.render(scene, metaCam);
 
 		stats.update();
@@ -186,10 +242,5 @@ async function logDxfInfo() {
 	allTypes.forEach((type) => {
 		allPossibleTypes.add(type);
 	});
-	console.log('allPossibleTypes', allPossibleTypes);
-
-	const target = dxf.entities.filter((entity) => entity.type === 'HATCH');
-	console.log('target.length', target.length);
-	console.log('target', target);
 }
 logDxfInfo();
